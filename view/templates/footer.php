390ccba0d95364c22cf8bb72046e3002d73550fd
furuyanatsumi <ntm.fry92@gmail.com>
2022-03-04 00:09:02 +0900

    アップロード

--- view/templates/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <p><small>&copy; 2022 beanshop</small></p>
-    <form action="./logout.php" method="post">
-    <div class="logout"><input type="submit" value="ログアウト"></div>
-    </form>
-</footer>
--- view/templates/header.php
+++
@@ -1,3 +0,0 @@
-<header class="page_header">
-    <h1><a href="../../controller/index.php"><img class="logo" src="./images/logo.png" alt="logo"></a></h1>    
-</header>
